ea17457f881034d8c28a47cfc795c145aac8a20f
Andrew J. Hesford <ajh@sideband.org>
2009-12-09 19:36:03 -0500

    Move the coordinate conversion routines to geom.py

--- pyajh/cutil.py
+++ pyajh/cutil.py
@@ -53,39 +53,3 @@ def mse (x, y):
 	err /= numpy.sum (numpy.abs(y)**2)
 
 	return math.sqrt(err)
-
-def sph2cart (r, t, p):
-	'''
-	Mimics the sph2cart function of Matlab, converting spherical
-	coordinates (r, t, p) to Cartesian coordinates (x, y, z). The
-	variables t and p are the polar and azimuthal angles, respectively.
-	'''
-	st = numpy.sin(t)
-	return r * st * numpy.cos(p), r * st * numpy.sin(p), r * numpy.cos(t)
-
-def cart2sph (x, y, z):
-	'''
-	Mimics the cart2sph function of Matlab, converting Cartesian
-	coordinates (x, y, z) to spherical coordinates (r, t, p). The
-	variables t and p are the polar and azimuthal angles, respectively.
-	'''
-	r = numpy.sqrt(x**2 + y**2 + z**2)
-	t = numpy.arccos (z / r)
-	p = numpy.arctan2 (y, x)
-	return r, t, p
-
-def pol2cart (r, t):
-	'''
-	Mimics the pol2cart function of Matlab, converting polar coordinates
-	(r, t) to Cartesian coordinates (x, y).
-	'''
-	return r * numpy.cos(t), r * numpy.sin(t)
-
-def cart2pol (x, y):
-	'''
-	Mimics the cart2pol function of Matlab, converting Cartesian
-	coordinates (x, y) to polar coordinates (r, t).
-	'''
-	r = numpy.sqrt (x**2 + y**2)
-	t = numpy.arctan2 (y, x)
-	return r, t
--- pyajh/geom.py
+++ pyajh/geom.py
@@ -26,3 +26,39 @@ def cyleqvrad(v, h):
 	'''
 
 	return math.sqrt(v / math.pi / h)
+
+def sph2cart (r, t, p):
+	'''
+	Mimics the sph2cart function of Matlab, converting spherical
+	coordinates (r, t, p) to Cartesian coordinates (x, y, z). The
+	variables t and p are the polar and azimuthal angles, respectively.
+	'''
+	st = numpy.sin(t)
+	return r * st * numpy.cos(p), r * st * numpy.sin(p), r * numpy.cos(t)
+
+def cart2sph (x, y, z):
+	'''
+	Mimics the cart2sph function of Matlab, converting Cartesian
+	coordinates (x, y, z) to spherical coordinates (r, t, p). The
+	variables t and p are the polar and azimuthal angles, respectively.
+	'''
+	r = numpy.sqrt(x**2 + y**2 + z**2)
+	t = numpy.arccos (z / r)
+	p = numpy.arctan2 (y, x)
+	return r, t, p
+
+def pol2cart (r, t):
+	'''
+	Mimics the pol2cart function of Matlab, converting polar coordinates
+	(r, t) to Cartesian coordinates (x, y).
+	'''
+	return r * numpy.cos(t), r * numpy.sin(t)
+
+def cart2pol (x, y):
+	'''
+	Mimics the cart2pol function of Matlab, converting Cartesian
+	coordinates (x, y) to polar coordinates (r, t).
+	'''
+	r = numpy.sqrt (x**2 + y**2)
+	t = numpy.arctan2 (y, x)
+	return r, t
