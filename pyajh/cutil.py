ddc60f864e64de6f073b599e7165449048221b7d
Andrew J. Hesford <ajh@sideband.org>
2010-08-10 14:37:07 -0400

    Add functions to compute FMM translators, Legendre polynomials and the excess bandwidth formula

--- pyajh/cutil.py
+++ pyajh/cutil.py
@@ -5,6 +5,76 @@ General-purpose numerical routines used in other parts of the module.
 import numpy
 import math
 from scipy import special as spec
+from pyajh.geom import sph2cart
+
+def translator (r, s, phi, theta, l):
+	'''
+	Compute the diagonal translator for a translation distance r, a
+	translation direction s, azimuthal samples specified in the array phi,
+	polar samples specified in the array theta, and a truncation point l.
+	'''
+
+	# The radial argument
+	kr = 2. * math.pi * r
+
+	# Compute the radial component
+	hl = spec.sph_jn(l, kr)[0] + 1j * spec.sph_yn(l, kr)[0]
+
+	# Define a dot-product function between the translation direction
+	# and the sample direction with specified angular coordinates
+	sd = lambda t, p: numpy.dot(s, sph2cart(1., t, p))
+
+	# Compute the translator at the specified angular samples
+	tr = [[sum([(1j)**li * (2. * li + 1.) * v * h
+		for li, (h, v) in enumerate(zip(hl, legpoly(sd(th, ph), l)))])
+		for th in theta] for ph in phi]
+
+	return tr
+
+def legpoly (x, n = 0):
+	'''
+	A coroutine to generate the Legendre polynomials up to order n,
+	evaluated at argument x.
+	'''
+
+	if abs(x) > 1.0:
+		raise ValueError("Argument must be in the range [-1,1]")
+	if n < 0:
+		raise ValueError("Order must be nonnegative")
+
+	# Yield the zero-order value
+	yield 1.0
+
+	# Yield the first-order value, if that order is desired
+	if n < 1: raise StopIteration
+	else: yield x
+
+	# Set some recursion values
+	cur = x
+	prev = 1.0
+
+	# Now compute the subsequent values
+	for i in xrange(1, n):
+		next = ((2. * i + 1.) * x * cur - i * prev) / (i + 1.)
+		prev = cur
+		cur = next
+		yield cur
+
+	raise StopIteration
+
+
+def exband (a, tol = 1e-6):
+	'''
+	Compute the excess bandwidth estimation for an object with radius a
+	to a tolerance tol.
+	'''
+
+	# Compute the number of desired accurate digits
+	d0 = -math.log10(tol)
+
+	# Return the estimated bandwidth
+	return int(2. * math.pi * a + 1.8 * (d0**2 * 2. * math.pi * a)**(1./3.))
+
 
 def gaussleg (m, tol = 1e-9):
 	'''
