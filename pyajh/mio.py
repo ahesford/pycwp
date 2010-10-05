ccfdd55ac7a40be650c1bfb85a2f1a0b57918179
Andrew J. Hesford <ajh@sideband.org>
2010-10-05 15:35:57 -0400

    Improve the readbmat function to avoid multiple reads for type matching. Types are now determined using file size and data type size.

--- pyajh/mio.py
+++ pyajh/mio.py
@@ -4,6 +4,7 @@ Routines for reading and writing binary matrix files in FORTRAN-order.
 
 import numpy
 import math
+import os
 
 def writebmat (mat, fname):
 	'''
@@ -20,25 +21,37 @@ def writebmat (mat, fname):
 	mat.flatten('F').tofile (outfile)
 	outfile.close ()
 
-def readbmat (fname, dimen = 2, type = None, size = None):
+def readbmat (fname, dimen = 2, dtype = None, size = None):
 	'''
 	Read a binary, complex matrix file, auto-sensing the precision
 	'''
-	# If the type was not provided, try a sequence of values
-	if type is None:
-		# The default list of types to try
-		typelist = (numpy.complex128, numpy.complex64,
-				numpy.float64, numpy.float32)
-		# Try each type and short-circuit if the correct one is found
-		for tp in typelist:
-			data = readbmat(fname, dimen, tp, size)
-			if data is not None: return data
 
 	# The type was provided, so do the read
 	infile = open (fname, mode='rb')
 	
 	# Read the dimension specification from the file
 	dimspec = numpy.fromfile (infile, dtype=numpy.int32, count=dimen)
+
+	# A dictionary describing auto-sensed data types.
+	datatypes = { numpy.float32().nbytes : numpy.float32,
+			numpy.complex64().nbytes : numpy.complex64, 
+			numpy.complex128().nbytes : numpy.complex128 }
+
+	# Attempt to determine the data type.
+	if dtype is None:
+		# Grab the file size, minus the header size
+		fsize = os.stat(fname)[6] - 4 * dimen
+		# Grab the number of bytes per record
+		nbytes = fsize / numpy.prod(dimspec)
+		# Make sure the sizes match up
+		if nbytes * numpy.prod(dimspec) != fsize:
+			raise TypeError('Could not determine data type in file.')
+		# Grab the requested data type or fail
+		try:
+			dtype = datatypes[nbytes]
+		except KeyError:
+			raise TypeError('Could not determine data type in file.')
+
 	# Override the read size with the provided size
 	if size is not None: dimspec = size
 	
@@ -46,11 +59,12 @@ def readbmat (fname, dimen = 2, type = None, size = None):
 	nelts = numpy.prod (dimspec)
 
 	# Read the number of elements provided and close the file
-	data = numpy.fromfile (infile, dtype = type, count = nelts)
+	data = numpy.fromfile (infile, dtype = dtype, count = nelts)
 	infile.close()
 
-	# The read failed to capture all requested elements, return nothing
-	if data.size != nelts or data is None: return None
+	# The read failed to capture all requested elements, raise an exception
+	if data.size != nelts or data is None:
+		return ValueError('Failed to read requested data.')
 	
 	# Rework the array as a numpy array with the proper shape
 	# Fortran order is assumed
