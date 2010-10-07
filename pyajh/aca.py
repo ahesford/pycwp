66becbee26aa9e89a0c797b81360f56056e934ec
Andrew J. Hesford <ajh@sideband.org>
2010-10-07 12:15:28 -0400

    Bump version and add recompression to ACA.

--- pyajh/aca.py
+++ pyajh/aca.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.linalg as la
 import math
 
 from itertools import izip
@@ -68,4 +69,37 @@ def matrix(eltfunc, nr, nc, tol = 1e-6):
 		# Store the convergence history of the ACA
 		zhist.append(math.sqrt(nrmsq / zabs))
 
-	return np.array(u), np.array(v), zhist
+	return np.array(u).transpose(), np.array(v).transpose().conjugate(), zhist
+
+def recompress(u, v, tol = 1e-6):
+	'''
+	Recompress a reduced-rank factorization of a matrix A = U * V^H.
+	The factorized matrices u and v are decomposed and truncated to
+	a rank that matches the specified tolerance.
+	'''
+
+	# Compute the QR decompositions of the factor matrices
+	qu, ru = la.qr(u)
+	qv, rv = la.qr(v)
+
+	# Compute the outer product of the triangular matrices
+	rp = np.dot(ru, rv.transpose().conjugate())
+
+	# Compute the singular value decomposition of the triangular product
+	uhat, shat, vhat = la.svd(rp)
+
+	# Find the approximate rank of the inner matrix
+	try: rank = (np.abs(shat / np.max(shat)) < tol).tolist().index(True) + 1
+	except: rank = len(shat)
+
+	# Truncate the row space
+	uhat = uhat[:,:rank]
+
+	# Truncate and multiply the diagonal matrix by the column space
+	vhat = np.array([sv * vv for sv, vv in izip(shat[:rank], vhat[:rank])])
+
+	# Compute the modified row and column matrices
+	ut = np.dot(qu, uhat)
+	vt = np.dot(qv, vhat.transpose().conjugate())
+
+	return ut, vt
