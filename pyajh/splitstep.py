dd111084f74b92c5d734d5aa03a77a2a84c4ae96
Andrew Hesford <ajh@or4.url.rochester.edu>
2010-12-28 21:33:49 -0500

    Simplify split-step calculations and add a slab concatenation script.

--- pyajh/splitstep.py
+++ pyajh/splitstep.py
@@ -5,6 +5,7 @@ Routines to compute split-step propagation through slices of media.
 import numpy as np
 import numpy.fft as fft
 import math
+from pyajh import mio
 
 def slicecoords(nx, ny, dx):
 	'''
@@ -89,3 +90,61 @@ def propfield(fld, prop):
 	'''
 
 	return fft.ifftn(prop * fft.fftn(fld))
+
+def compguess(k0, dx, inc, atn, objfile, fmt = 'SplitStep.%03d.field'):
+	'''
+	Propagate the incident field inc, with wave number k0, through a
+	contrast specified in objfile with cell size dx. The slab closest to
+	the incident field is the last slab in the contrast file. Each slab has
+	an attenuation profile atn to avoid boundary reflections. Output is
+	written in slab files with name format fmt.
+	'''
+
+	# Determine the contrast dimensions and see if the incident field matches
+	hdr, dtype = mio.getmattype(objfile)
+	# Note the position of the start of data
+	fpos = objfile.tell()
+
+	# The number of pixels per slab
+	npix = hdr[0] * hdr[1]
+
+	# Record the size of the computational grid
+	pgrid = list(inc.shape[:])
+
+	# Check that the computational grid is at least as large as the contrast
+	if hdr[0] > pgrid[0] or hdr[1] > pgrid[1]:
+		raise ValueError('Contrast must not be larger than computation grid.')
+
+	# Record the offsets for zero-padding contrast grids
+	xmin, ymin = (pgrid[0] - hdr[0]) / 2, (pgrid[1] - hdr[1]) / 2
+	xmax, ymax = xmin + hdr[0], ymin + hdr[1]
+
+	# Generate the propagator
+	prop = genprop (k0, pgrid[0], pgrid[1], dx)
+
+	# Write the incident field as the first slab in the desired data type
+	mio.writebmat(np.array(inc[xmin:xmax,ymin:ymax], dtype = dtype), fmt % hdr[2])
+	print('Wrote ' + fmt % hdr[2])
+
+	# Copy the incident field
+	fld = inc.copy()
+
+	# Read the contrast slab, propagate and write the field
+	for idx in range(hdr[2] - 1, -1, -1):
+		# Create a padded slab and read the contrast values
+		obj = np.zeros(pgrid, dtype = dtype)
+		objfile.seek(fpos + npix * idx * dtype().nbytes)
+		obj[xmin:xmax,ymin:ymax] = np.fromfile(objfile,
+				dtype=dtype, count=npix).reshape(hdr[:2], order='F')
+
+		# Generate the phase screen and propagate the field
+		# Make sure to cast into the expected format
+		screen = genscreen (obj, k0, dx)
+		fld = propfield (fld, prop)
+		fld *= screen * atn
+
+		# Write the slab in the desired data type
+		mio.writebmat(np.array(fld[xmin:xmax,ymin:ymax], dtype=dtype), fmt % idx)
+		print('Wrote ' + fmt % idx)
+
+	return hdr[2]
---
+++ shell/catslab.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys
+import getopt
+import numpy as np
+
+from pyajh import mio
+
+def usage (progname = 'mse.py'):
+	print >> sys.stderr, "Usage: %s [-h] [-t xl,xh,yl,yh] <slab1> [...] <slabN>" % progname
+
+def main (argv = None):
+	if argv is None:
+		argv = sys.argv[1:]
+		progname = sys.argv[0]
+
+	optlist, args = getopt.getopt (argv, 'ht:')
+	trunc = False
+
+	# Parse the option list
+	for opt in optlist:
+		if opt[0] == '-t':
+			trunc = True
+			xmin, xmax, ymin, ymax = tuple([int(l) 
+				for l in opt[1].split(',')])
+		else:
+			usage(progname)
+			return 128
+
+	# There must be at least two files to concatenate
+	if len(args) < 2:
+		usage (progname)
+		return 128
+
+	# Grab the number of slabs, the slab dimensions
+	# and data type from the first file.
+	nslabs = len(args)
+
+	# Grab the first slab and its data type, truncating if necessary
+	slab = mio.readbmat(args.pop(0))
+	if trunc: slab = slab[xmin:xmax,ymin:ymax]
+	dtype = slab.dtype
+
+	# Get the overall matrix size
+	shape = list(slab.shape)
+	shape.append(nslabs)
+
+	# Write the matrix size to the output
+	np.array(shape, dtype=np.int32).tofile(sys.stdout)
+
+	# Write the first slab to the output
+	slab.flatten('F').tofile(sys.stdout)
+
+	# Loop through all remaining slabs, converting data types along the way
+	for name in args:
+		slab = np.array(mio.readbmat(name), dtype=dtype)
+		if trunc: slab = slab[xmin:xmax,ymin:ymax]
+		slab.flatten('F').tofile(sys.stdout)
+
+	sys.stdout.close()
+	return 0
+
+if __name__ == "__main__":
+	sys.exit (main ())
