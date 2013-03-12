e27adae05e08134c199f05f071f1e886fdb09f65
Andrew J. Hesford <ajh@sideband.org>
2013-03-11 21:58:47 -0400

    Look for FFTW_DIR as a library prefix when building F2PY modules.

--- pyajh/f2py/setup.py
+++ pyajh/f2py/setup.py
@@ -1,12 +1,22 @@
 #!/usr/bin/env python
 def configuration(parent_package='', top_path=None):
 	from numpy.distutils.misc_util import Configuration
+	import os, os.path
+
+	# This lists prefixes for library dependencies
+	prefixes = ['/usr/local']
+	# Try to add FFTW_DIR to the prefixes
+	try: prefixes.append(os.environ['FFTW_DIR'])
+	except KeyError: pass
+
+	# Buidl the include and library directories lists
+	include_dirs = [os.path.join(p, 'include') for p in prefixes]
+	library_dirs = [os.path.join(p, 'lib') for p in prefixes]
 
 	config = Configuration('f2py', parent_package, top_path)
 	config.add_extension('pade', sources=['pade.f'],)
 	config.add_extension('splitstep', sources=['splitstep.f90'],
-			include_dirs=['/usr/local/include'], 
-			library_dirs=['/usr/local/lib'],
+			include_dirs=include_dirs, library_dirs=library_dirs,
 			libraries=['fftw3f_threads', 'fftw3f'],)
 
 	return config
