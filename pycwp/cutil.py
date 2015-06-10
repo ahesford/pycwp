5ace6ef7405dac3d8accb07ef58f8614d366d562
Andrew J. Hesford <ajh@sideband.org>
2015-06-10 10:48:38 -0400

    Improve filtering options in cutil.findpeaks

--- pycwp/cutil.py
+++ pycwp/cutil.py
@@ -9,7 +9,7 @@ from itertools import izip, count
 
 from pycwp.util import alternator
 
-def findpeaks(vec, minwidth=None, minprom=None):
+def findpeaks(vec, minwidth=None, minprom=None, prommode='absolute'):
 	'''
 	Find all peaks in the 1-D sequence vec. Return a list with elements
 	(index, value, width, prominence) for a peak at the indicated index
@@ -17,8 +17,16 @@ def findpeaks(vec, minwidth=None, minprom=None):
 	its key col, and prominence is the peak's height above its key col.
 
 	Only peaks with widths greater than a specified minwidth, or with
-	prominences greater than the specified minprom, will be considered.
+	prominences greater than the specified minprom, will be considered. If
+	prommode is 'absolute' or 'abs', minprom is an absolute cutoff. If
+	prommode is 'relative' or 'rel', minprom is interpreted as a fraction
+	of the highest prominence.
 	'''
+	try:
+		absprom = {'abs': True, 'absolute': True,
+				'rel': False, 'relative': False}[prommode.lower()]
+	except KeyError: raise ValueError('Invalid argument for prommode')
+
 	# Prepare output extrema
 	maxtab, mintab = [], []
 
@@ -97,6 +105,28 @@ def findpeaks(vec, minwidth=None, minprom=None):
 	# Determine the overall minimum value
 	minval = min(ex[1] for ex in mintab)
 
+	if minprom and not absprom:
+		# Reinterpret minprom if relative mode was specified
+		minprom *= (max(ex[1] for ex in maxtab) - minval)
+
+	if minwidth and minprom:
+		# Assess both prominence and width for cuttof
+		def goodpeak(width, prom):
+			return (width >= minwidth) and (prom >= minprom)
+	elif minwidth:
+		# Assess only width for cutoff
+		def goodpeak(width, prom):
+			return width >= minwidth
+	elif minprom:
+		# Assess only prominence for cutoff
+		def goodpeak(width, prom):
+			return prom >= minprom
+	else:
+		# There is no cutoff
+		def goodpeak(width, prom):
+			return True
+
+
 	# Build the peak list
 	peaks = []
 	for (ei, ev), kcol in izip(maxtab, keycols):
@@ -110,10 +140,9 @@ def findpeaks(vec, minwidth=None, minprom=None):
 			# The width stretches all the way to the end of the signal
 			width = max(ei, len(vec) - ei)
 			prom = ev - minval
-		# Skip recording peaks that don't satisfy minimum requirements
-		if minwidth and (width < minwidth): continue
-		if minprom and (prom < minprom): continue
-		peaks.append((ei, ev, width, prom))
+		# Only record "good" peaks
+		if goodpeak(width, prom):
+			peaks.append((ei, ev, width, prom))
 
 	return peaks
 
