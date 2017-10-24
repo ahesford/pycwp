e805da2338b10c89b9b0b4c55d8027961fc755c6
Andrew J. Hesford <ajh@sideband.org>
2017-10-24 07:58:48 -0400

    Changes according to 2to3.

--- pycwp/cltools/__init__.py
+++ pycwp/cltools/__init__.py
@@ -3,8 +3,8 @@
 # Copyright (c) 2015 Andrew J. Hesford. All rights reserved.
 # Restrictions are listed in the LICENSE file distributed with this package.
 
-from util import *
-from wavecl import *
-from interpolators import *
+from .util import *
+from .wavecl import *
+from .interpolators import *
 
-__all__ = filter(lambda s: not s.startswith('_'), dir())
+__all__ = [s for s in dir() if not s.startswith('_')]
