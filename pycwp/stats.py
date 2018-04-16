1e0136fa7c02e42bae4b5f137249a24cfed93689
Andrew J. Hesford <ajh@sideband.org>
2018-04-15 22:23:30 -0400

    Add expanding modes to stats.rolling_* functions.

--- pycwp/stats.py
+++ pycwp/stats.py
@@ -68,61 +68,95 @@ def rolling_window(x, n):
 	return numpy.lib.stride_tricks.as_strided(x, shape=shape, strides=strides)
 
 
-def _largest_type(dtype):
+def _validate_rolling_inputs(x, n):
 	'''
-	If dtype is a complex datatype, return numpy.complex128. Otherwise,
-	return numpy.float64.
+	Verify that n is None or a positive integer and x can be represented as
+	a Numpy array, then return (xa, nv, dtype), where xa is the array
+	representation of x; nv is len(xa) if n is None, n otherwise; and dtype
+	is complex128 when xa.dtype is a complex type, float64 otherwise.
 	'''
-	if numpy.issubdtype(dtype, numpy.complexfloating):
-		return numpy.dtype('complex128')
-	else: return numpy.dtype('float64')
+	if n is not None and (n < 1 or n != int(n)):
+		raise ValueError('Value "n" must be None a nonnegative integer')
+
+	x = numpy.asarray(x)
+	if n is None: n = len(x)
+
+	if numpy.issubdtype(x.dtype, numpy.complexfloating):
+		dtype = numpy.dtype('complex128')
+	else: dtype = numpy.dtype('float64')
+	return x, n, dtype
 
 
-def rolling_mean(x, n):
+def rolling_mean(x, n, expand=False):
 	'''
 	Compute the rolling mean of length n along x as a flattened Numpy
 	array.
+
+	When expand=False, only the valid region of the rolling mean is
+	returned; for R = rolling_mean(x, n, expand=False),
+	
+	  R[i] = mean(x[i:n+i]), 0 <= i < len(x) - n + 1.
+
+	When expand=True and R = rolling_mean(x, n, expand=True),
+
+	  R[i] = mean(x[:i+1]) for 0 <= i < n,
+	  R[i] = mean(x[i-n:i]) for n <= i < len(x).
 	'''
-	if n < 1: raise ValueError('Value of n must be nonnegative')
-	x = numpy.asarray(x)
-	dtype = _largest_type(x.dtype)
+	x, n, dtype = _validate_rolling_inputs(x, n)
 
 	mx = numpy.mean(x, dtype=dtype)
 	ca = numpy.cumsum(x - mx, dtype=dtype)
 	ca[n:] -= ca[:-n]
-	return (mx + ca[n - 1:] / n).astype(x.dtype, copy=False)
+
+	nm1 = n - 1
+	ca[nm1:] = mx + ca[nm1:] / n
+
+	if not expand: return ca[nm1:].astype(x.dtype, copy=False)
+
+	# In "expand" mode, use expanding window before validity region
+	ca[:nm1] = mx + ca[:nm1] / numpy.arange(1, n)
+	return ca.astype(x.dtype, copy=False)
 
 
-def rolling_var(x, n):
+def rolling_var(x, n, expand=False):
 	'''
-	Compute the variance, over a sliding window of length n, along x as a
-	flattened Numpy array.
+	Performs rolling computations as described in rolling_mean, except the
+	variance is computed in place of the mean.
 	'''
-	if n < 1: raise ValueError('Value of n must be nonnegative')
-	x = numpy.asarray(x)
-	dtype = _largest_type(x.dtype)
+	x, n, dtype = _validate_rolling_inputs(x, n)
 
+	# Compute cumulative mean and mean squared
 	mx = numpy.mean(x, dtype=dtype)
 	ca = numpy.cumsum(x - mx, dtype=dtype)
 	cb = numpy.cumsum((x - mx)**2, dtype=dtype)
 
+	# Convert cumulative values to rolling values
 	ca[n:] -= ca[:-n]
 	cb[n:] -= cb[:-n]
 
-	return ((cb[n - 1:] - ca[n - 1:]**2 / n) / n).astype(x.dtype, copy=False)
+	# Convert rolling mean-squared values to variance in valid region
+	nm1 = n - 1
+	cb[nm1:] = (cb[nm1:] - ca[nm1:]**2 / n) / n
+
+	if not expand: return cb[nm1:].astype(x.dtype, copy=False)
+
+	# In "expand" mode, use expand window before validity region
+	nv = numpy.arange(1, n)
+	cb[:nm1] = (cb[:nm1] -  ca[:nm1]**2 / nv) / nv
+	return cb.astype(x.dtype, copy=False)
 
 
-def rolling_std(x, n):
+def rolling_std(*args, **kwargs):
 	'''
-	Compute the standard deviation, over a sliding window of length n,
-	along x as a flattened Numpy array.
+	Convenience function to compute the rolling standard deviation of x as
+	sqrt(rolling_var(*args, **kwargs)).
 	'''
-	return numpy.sqrt(rolling_var(x, n))
+	return numpy.sqrt(rolling_var(*args, **kwargs))
 
 
-def rolling_rms(x, n):
+def rolling_rms(x, *args, **kwargs):
 	'''
-	Compute the RMS value, over a sliding window of length n, along the
-	last axis of x.
+	Convenience function to compute the rolling RMS of x as
+	sqrt(rolling_mean(x**2, *args, **kwargs)).
 	'''
-	return numpy.sqrt(rolling_mean(x**2, n))
+	return numpy.sqrt(rolling_mean(x**2, *args, **kwargs))
